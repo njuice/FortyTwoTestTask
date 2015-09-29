c5c234512eee5923704554aad2eb4c6d975e0b0c
Bogdan Piven <bogdanpiven@gmail.com>
2015-09-29 22:42:38 +0300

    ticket:1 landing page with using angularjs

--- apps/landing/views.py
+++ apps/landing/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def index(request):
+    """
+        View for index page
+        :param request:
+        :return:
+    """
+    if request.user.is_authenticated():
+        return redirect('/tasks/')
+    else:
+        template = loader.get_template('landing/index.html')
+        context = RequestContext(request)
+        return HttpResponse(template.render(context))
---
+++ static/js/landing.js
@@ -0,0 +1,20 @@
+(function(){
+    var app = angular.module('landing', []);
+
+    app.directive('landingPage', function(){
+        return {
+            restrict: 'E',
+            templateUrl: '/static/html/landing_page.html',
+            controller: function($scope){
+                $scope.desc = 'Tasks MG - is the modern wev tasks management service for create/manage tasks and build own team';
+                $scope.about = 'My name is Bogdan Piven. I am a 24 years old, and I am fond of sports and web - developing.'
+                $scope.features = [
+                    'Manage your tasks',
+                    'Set deadline for tasks',
+                    'Build your team via Facebook',
+                    'Assign/re-assign tasks to your teammates'
+                ];
+            }
+        }
+    });
+})();
