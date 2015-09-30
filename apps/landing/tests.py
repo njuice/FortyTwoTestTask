c74c88976e29c23da6597356d6fa870dd39b3957
Bogdan Piven <bogdanpiven@gmail.com>
2015-09-30 23:44:28 +0300

    ticket:1 Simple landing page view test wad added

--- apps/landing/tests.py
+++ apps/landing/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import AnonymousUser, User
+from landing.views import index
 
-# Create your tests here.
+
+class LandingViewTests(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='tester', email='tester@whitehouse.gov',
+            password='top_secret')
+
+    def test_index_view(self):
+        """
+            Response status must be 200.
+            Page must contain dummy call-to-action button
+            Show page only for non-authorized users
+        """
+        # Show page only for non-authorized users
+        request = self.factory.get(reverse('landing:index'))
+        request.user = AnonymousUser()
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Sign in with Facebook')
+
+        # Redirect for authorized user
+        request = self.factory.get(reverse('landing:index'))
+        request.user = self.user
+        response = index(request)
+        self.assertEqual(response.status_code, 302)
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -5,5 +5,5 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('apps.landing.urls')),
+    url(r'^', include('apps.landing.urls', namespace="landing")),
 )
